41455268ce73ccb1408104512533e8c3d272c0bc
Lukas Micke <lmicke@techfak.uni-bielefeld.de>
2018-02-02 12:32:38 +0100

    added functions for Filch

---
+++ small_parsimony/Fitch/fitch.go
@@ -0,0 +1,32 @@
+package fitch
+
+type Tree struct {
+	Left *Tree
+	Right *Tree
+	Value string
+	Values []string
+	Parent *Tree
+}
+
+func merge_childs(tree Tree) {
+
+}
+
+func choose_taxa(tree Tree) {
+
+}
+
+func bottom_up(tree Tree){
+
+}
+
+func top_down(tree Tree) {
+
+}
+
+func union(a, b string[]) string[] {
+}
+
+func intersection(a, b string[]) string[] {
+
+}
